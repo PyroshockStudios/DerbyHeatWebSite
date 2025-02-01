f98030237e0d4bf043add04b6f50a11e65711995
TJ-LOK-TRL <2221481@my.ipleiria.pt>
2025-02-01 18:29:33 +0000

    setup

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,7 +4,7 @@ import { GitContentSource } from "@stackbit/cms-git";
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
   cmsName: "contentful",
-  nodeVersion: "16",
+  nodeVersion: "20",
   ssgName: "custom",
   devCommand: "npm run dev -- -p {PORT}",
   experimental: {
