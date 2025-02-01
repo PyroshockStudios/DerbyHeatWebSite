fbed3be09ee431acf27f9be7115021e1b207e382
TJ-LOK-TRL <2221481@my.ipleiria.pt>
2025-02-01 18:06:19 +0000

    setup

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,7 +4,7 @@ import { GitContentSource } from "@stackbit/cms-git";
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'nuxt',
-  nodeVersion: '11.1.0',
+  nodeVersion: '20',
   modelExtensions: [
     {
       name: "Page", 
