4741ff70414e2bf0950bd5d54e98511f2c3ca37c
TJ-LOK-TRL <2221481@my.ipleiria.pt>
2025-02-02 10:48:14 +0000

    test slug

---
+++ pages/[slug].vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ page.title }}</h1>
+        <p>{{ page.content }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const page = ref({ title: '', content: '' });
+
+const route = useRoute();
+const slug = route.params.slug;
+
+onMounted(async () => {
+    try {
+        const response = await fetch(`/content/pages/${slug}.json`);
+        if (response.ok) {
+            page.value = await response.json();
+        } else {
+            throw new Error('Página não encontrada');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+</script>
